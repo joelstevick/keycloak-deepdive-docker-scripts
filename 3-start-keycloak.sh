d67c1482064e5a009d52d7dd4029b64be8ee1da8
Joel Stevick <joelstevick@outlook.com>
2024-10-10 06:31:41 -0400

    stop container if exists

--- 3-start-keycloak.sh
+++ 3-start-keycloak.sh
@@ -13,23 +13,31 @@ KC_DB_URL="jdbc:postgresql://host.docker.internal:5432/keycloak"
 KC_DB_USERNAME="keycloak"
 KC_DB_PASSWORD="keycloak"
 
-# Check if Keycloak container is already running
+# Check if the Keycloak container is already running
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-    echo "Keycloak container '$CONTAINER_NAME' is already running."
-else
-    # Start Keycloak container in detached mode
-    echo "Starting Keycloak container..."
-    docker run -d \
-      --name $CONTAINER_NAME \
-      -p 8080:8080 \
-      -e KEYCLOAK_ADMIN=$KEYCLOAK_ADMIN \
-      -e KEYCLOAK_ADMIN_PASSWORD=$KEYCLOAK_ADMIN_PASSWORD \
-      -e KC_DB=$KC_DB \
-      -e KC_DB_URL=$KC_DB_URL \
-      -e KC_DB_USERNAME=$KC_DB_USERNAME \
-      -e KC_DB_PASSWORD=$KC_DB_PASSWORD \
-      quay.io/keycloak/keycloak \
-      start-dev
-
-    echo "Keycloak container '$CONTAINER_NAME' started successfully."
+    echo "Stopping and removing running Keycloak container..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
 fi
+
+# Check if the container exists but is not running (and remove it)
+if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Removing the existing Keycloak container..."
+    docker rm $CONTAINER_NAME
+fi
+
+# Start Keycloak container in detached mode
+echo "Starting Keycloak container..."
+docker run -d \
+  --name $CONTAINER_NAME \
+  -p 8080:8080 \
+  -e KEYCLOAK_ADMIN=$KEYCLOAK_ADMIN \
+  -e KEYCLOAK_ADMIN_PASSWORD=$KEYCLOAK_ADMIN_PASSWORD \
+  -e KC_DB=$KC_DB \
+  -e KC_DB_URL=$KC_DB_URL \
+  -e KC_DB_USERNAME=$KC_DB_USERNAME \
+  -e KC_DB_PASSWORD=$KC_DB_PASSWORD \
+  quay.io/keycloak/keycloak \
+  start-dev
+
+echo "Keycloak container '$CONTAINER_NAME' started successfully."
